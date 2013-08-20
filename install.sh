53d94637fa2d3bb6e07f431ba4cfe0d52a3af44d
Stuart King <stuartrexking@gmail.com>
2013-08-19 18:14:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 mkdir -p ~/bin
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/git-completion.bash
+curl -sL https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > ~/bin/git-completion.bash
+
+curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ~/bin/vcprompt
+chmod 755 ~/bin/vcprompt
 
 mkdir -p ~/Workspace
 
